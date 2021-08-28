84c607122f50d6ef7db3a078d098d7f05f69434d
Pushp Jain <pushp.ug20@nsut.ac.in>
2021-08-29 04:06:12 +0530

    uff

--- frontend/src/Pages/IsTriangle.js
+++ frontend/src/Pages/IsTriangle.js
@@ -3,7 +3,18 @@ import React from 'react'
 function IsTriangle() {
     return (
         <div>
-            Check Triangle
+            <h3>Home</h3>
+            <div>
+                Check if the shape is a valid triangle: 
+            </div>
+            <div className="angles-input">
+                <label htmlFor="angle-a">Angle A</label>
+                <input id="angle-a" type="number" />
+                <label htmlFor="angle-b">Angle B</label>
+                <input id="angle-b" type="number" />
+                <label htmlFor="angle-c">Angle C</label>
+                <input id="angle-c" type="number" />
+            </div>
         </div>
     )
 }
